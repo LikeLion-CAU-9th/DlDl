a89195beb5ecab629030b7eda9e9f828bbd9b35e
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-06-30 20:16:46 +0900

    clients/models.py import User

--- didi/clients/models.py
+++ didi/clients/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-
+from accounts.models import User
 class Enterprise(models.Model):
     # title, body, show, datetime, image
 
