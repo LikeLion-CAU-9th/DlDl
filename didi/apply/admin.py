d284b07faf7fbdce5ede23a93bd2cd063d854786
k-mover <armoire@gimtaeyeong-ui-MacBookPro.local>
2021-06-30 19:59:20 +0900

    feat:apply수정

--- didi/apply/admin.py
+++ didi/apply/admin.py
@@ -3,7 +3,6 @@ from apply.models import Apply, Project, ProjectComment
 
 # Register your models here.
 
-admin.site.register(Appl
-y)
+admin.site.register(Apply)
 admin.site.register(Project)
 admin.site.register(ProjectComment)
