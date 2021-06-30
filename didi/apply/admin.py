50f0722cb84b5b953f71d70504e58b24b9f58265
k-mover <armoire@gimtaeyeong-ui-MacBookPro.local>
2021-06-30 19:56:14 +0900

    feat:apply/admin.py 생성

--- didi/apply/admin.py
+++ didi/apply/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apply.models import Apply, Project, ProjectComment
 
 # Register your models here.
+
+admin.site.register(Appl
+y)
+admin.site.register(Project)
+admin.site.register(ProjectComment)
