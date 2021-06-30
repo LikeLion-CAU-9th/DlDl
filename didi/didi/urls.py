f5f3aa564d84079c59777cb60d4240fcfc80cbe7
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-06-30 20:14:24 +0900

    urls.py 수정

--- didi/didi/urls.py
+++ didi/didi/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,6 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('designers/', include('designers.urls')),
-    path('clients/', include('clients.urls')),   
+    path('designers/', include('designers.urls')),   
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
